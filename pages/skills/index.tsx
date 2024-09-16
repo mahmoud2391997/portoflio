346239d7234bc0cd028b0f16dea664614a89bb6e
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2024-09-16 22:15:52 +0300

    try to deploy

--- pages/skills/index.tsx
+++ pages/skills/index.tsx
@@ -1,6 +1,6 @@
 export default function TechnologiesPage() {
   return (
-    <div className="grid xl:grid-cols-4 md:grid-cols-2 grid-cols-1 my-36 xl:m-0 w-[70%] xl:max-h-[540px]  gap-6 text-white x">
+    <div className="grid xl:grid-cols-4 md:grid-cols-2 grid-cols-1 my-36 xl:m-0 w-[70%] xl:max-h-[540px]  gap-6 text-white">
       <section className="h-full bg-[#7c7c7c] rounded w-full flex flex-col justify-start p-3 relative">
         <h2 className="text-3xl w-full text-center mb-5">
           Creating Pixel Perfect UI As Figma Design
